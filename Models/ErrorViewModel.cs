3f79a354d31214fd035897711ae8cdb2fe85e6b0
Robert5957 <charles.chou570114@gmail.com>
2022-07-01 22:25:04 +0800

    加入專案檔案。

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MSIT141_24Site.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
